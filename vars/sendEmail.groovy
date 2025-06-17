695da0ba510d0f11e8e762a3a4a80eb76e9f79a7
Sanket Mule <sanketsp60@gmail.com>
2025-06-17 16:43:29 +0530

    Updated sendEmail.groovy

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,22 +1,31 @@
-// vars/sendEmail.groovy
-
 def call(Map config = [:]) {
     def isSuccess = config.get('isSuccess', false)
     def currentBuild = config.currentBuild
     def env = config.env
+    def message = config.get('message', '')
 
     def status = isSuccess ? "SUCCESS" : "FAILURE"
     def color = isSuccess ? "green" : "red"
     def icon = isSuccess ? "✅" : "❌"
-    def logExcerpt = isSuccess ? "" :
-        (currentBuild?.rawBuild?.getLog(100)?.join("<br/>") ?: "No logs available")
+
+    def stageInfo = !isSuccess ? """
+        <b>Failed Stage:</b> ${env.FAILED_STAGE ?: "Unknown"}<br/>
+        <b>Reason:</b> ${env.FAILURE_REASON ?: message}<br/>
+    """ : ""
+
+    def logExcerpt = isSuccess ? "" : """
+        <pre style='font-size:13px;background:#f9f9f9;border:1px solid #ccc;padding:10px;'>
+        ${currentBuild.rawBuild.getLog(50).join("<br/>")}
+        </pre>
+    """
 
     def body = """
         <h2 style='color:${color};'>Build ${status}</h2>
-        <b>Job:</b> ${env.JOB_NAME} <br/>
-        <b>Build Number:</b> #${env.BUILD_NUMBER} <br/>
-        <b>Duration:</b> ${currentBuild.durationString ?: 'N/A'} <br/>
+        <b>Job:</b> ${env.JOB_NAME}<br/>
+        <b>Build Number:</b> #${env.BUILD_NUMBER}<br/>
+        <b>Duration:</b> ${currentBuild.durationString}<br/>
         <b>URL:</b> <a href='${env.BUILD_URL}'>${env.BUILD_URL}</a><br/>
+        ${stageInfo}
         ${logExcerpt}
     """
 
